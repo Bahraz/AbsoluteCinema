984a197c612d8d6036096bfca22e0e4d98a55c7e
Jaroslaw <jarekg0626@gmail.com>
2025-11-09 22:23:03 +0100

    fix vercel

---
+++ src/vercel.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import serverless from 'serverless-http';
+import app from './app';
+import { connectDB } from '@config/db';
+
+dotenv.config();
+
+let initialized = false;
+
+async function bootstrap() {
+  if (!initialized) {
+    await connectDB();
+    initialized = true;
+  }
+}
+
+export default async function handler(req: any, res: any) {
+  await bootstrap();
+  const expressHandler = serverless(app);
+  return expressHandler(req, res);
+}
