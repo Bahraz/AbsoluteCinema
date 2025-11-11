0d994ea8c4df002c29790447835d3cb38c7b5b2c
Jaroslaw <jarekg0626@gmail.com>
2025-11-11 17:57:43 +0100

    test

--- src/routes/api/movieRoutes.ts
+++ src/routes/api/movieRoutes.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 // Endpoint do zarządzania filmami.
 router.get('/show', (req, res) => movieController.showMovies(req, res));
 router.get('/details/:title', (req, res) => movieController.showMovieDetails(req, res));
+router.get('/search', (req, res) => console.log('Search endpoint not implemented yet'));
 
 export default router;
 
