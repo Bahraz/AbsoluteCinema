0b5f3caaef9dedcf57a5f1388866aa7019dc5f2f
Jaroslaw <jarekg0626@gmail.com>
2025-11-11 17:32:25 +0100

    test

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import app from './app';
-import { connectDB } from '@config/database';
+import { connectDB } from './config/database';
 
 const PORT = process.env.PORT || 3000;
 
